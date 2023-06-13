ce8395cc1e39bcca21f570d1f0ebd59c30bd4011
Din Gilig <dinniel.gilig@module-zero.com>
2023-06-13 22:43:30 +0800

    updated code

--- MP3_HMM.py
+++ MP3_HMM.py
@@ -6,18 +6,15 @@ https://towardsdatascience.com/hidden-markov-model-implemented-from-scratch-7286
 https://medium.com/@kangeugine/hidden-markov-model-7681c22f5b9
 '''
 
-
 class Vector:
     def __init__(self, probabilities):
         self.states = list(probabilities.keys())
         self.df = probabilities
 
-
 class Matrix:
     def __init__(self, probabilities):
         self.states = list(probabilities.keys())
         self.df = probabilities
-
 
 class HiddenMarkovModel:
     def __init__(self, transition_matrix, emission_matrix, start):
@@ -32,8 +29,7 @@ class HiddenMarkovModel:
         chain_length = len(observations)
         for chain in product(self.states, repeat=chain_length):
             p_hidden_state = self.start.df[chain[0]]
-            p_observations = self.emission_matrix.df[chain[0]
-                                                     ].df[observations[0]]
+            p_observations = self.emission_matrix.df[chain[0]].df[observations[0]]
 
             for i in range(1, chain_length):
                 current_state = chain[i]
@@ -52,8 +48,7 @@ class HiddenMarkovModel:
         chain_length = len(observations)
         for chain in product(self.states, repeat=chain_length):
             p_hidden_state = self.start.df[chain[0]]
-            p_observations = self.emission_matrix.df[chain[0]
-                                                     ].df[observations[0]]
+            p_observations = self.emission_matrix.df[chain[0]].df[observations[0]]
 
             for i in range(1, chain_length):
                 current_state = chain[i]
@@ -70,17 +65,13 @@ class HiddenMarkovModel:
 
     def display_probabilities(self, observations_list):
         for observations in observations_list:
-            print("Score for {} is {:f}.".format(
-                observations, self.get_probability(observations)))
+            print("Score for {} is {:f}.".format(observations, self.get_probability(observations)))
 
     def display_optimal_sequence(self, observations_list):
         for observations in observations_list:
-            optimal_sequence, max_probability = self.get_optimal_hidden_sequence(
-                observations)
+            optimal_sequence, max_probability = self.get_optimal_hidden_sequence(observations)
 
-            print("Given the known model and the observation {}, the weather was most likely {} with ~{:0.2f}% probability.".format(
-                observations, optimal_sequence, max_probability * 100))
-
+            print("Given the known model and the observation {}, the weather was most likely {} with ~{:0.2f}% probability.".format(observations, optimal_sequence, max_probability * 100))
 
 def main():
     start = Vector({'Rainy': 0.6, 'Sunny': 0.4})
@@ -91,11 +82,9 @@ def main():
     observable_state1 = Vector({'Walk': 0.1, 'Shop': 0.4, 'Clean': 0.5})
     observable_state2 = Vector({'Walk': 0.6, 'Shop': 0.3, 'Clean': 0.1})
 
-    transition_matrix = Matrix(
-        {'Rainy': hidden_state1, 'Sunny': hidden_state2})
+    transition_matrix = Matrix({'Rainy': hidden_state1, 'Sunny': hidden_state2})
 
-    emission_matrix = Matrix(
-        {'Rainy': observable_state1, 'Sunny': observable_state2})
+    emission_matrix = Matrix({'Rainy': observable_state1, 'Sunny': observable_state2})
 
     hmm = HiddenMarkovModel(transition_matrix, emission_matrix, start)
 
